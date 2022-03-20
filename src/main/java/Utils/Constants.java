b93c5e29695d791e4615df7680b8e4b62a6ce274
ajinkya-deshmukh <ajinkya.deshmukh04@gmail.com>
2022-03-20 11:37:46 +0530

    Added constants to store some of the test data

--- src/main/java/Utils/Constants.java
+++ src/main/java/Utils/Constants.java
@@ -3,6 +3,14 @@ package Utils;
 
 public class Constants{
     public static String BASE_URL = "https://yocket.com/";
+    public static String MASTER_COUNTRY = "European Union";
+    public static String MASTER_SUBJECT = "Computer Science";
+    public static String UNIVERSITY_NAME = "University of Bremen";
+    public static String MAJOR_NAME = "Computer and Information Systems";
+    public static String GPA = "8.3";
+    public static String VALID_IELTS_SCORE = "6.5";
+    public static String WORK_EXP = "50";
+    public static String NUMBER_PROJECTS = "1";
 }
 
 
