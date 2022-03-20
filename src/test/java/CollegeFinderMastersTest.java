d85bd6399d3aa9bc666f4d7206b21b5684f9e324
ajinkya-deshmukh <ajinkya.deshmukh04@gmail.com>
2022-03-20 11:38:48 +0530

    Added college finders positive flow test

---
+++ src/test/java/CollegeFinderMastersTest.java
@@ -0,0 +1,33 @@
+import Pages.CollegeFinderPage;
+import Pages.HomePage;
+import Pages.UniversityResultsPage;
+import Utils.SpinUpBrowser;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+public class CollegeFinderMastersTest extends SpinUpBrowser{
+
+
+    HomePage homePage;
+    CollegeFinderPage collegeFinder;
+    UniversityResultsPage universityResultsPage;
+
+    @Test
+    public void verifyCollegeFinder(){
+        homePage = new HomePage(driver);
+        collegeFinder = new CollegeFinderPage(driver);
+        homePage.clickOnCollegeFinder();
+        collegeFinder.clickOnMasters();
+    }
+
+    @Test
+    public void findMastersCollegeTest() throws InterruptedException {
+        homePage = new HomePage(driver);
+        collegeFinder = new CollegeFinderPage(driver);
+        universityResultsPage = new UniversityResultsPage(driver);
+        collegeFinder.findMastersCollege();
+        universityResultsPage.verifyCollegeFinderSubmission();
+    }
+
+
+}
