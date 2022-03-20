3e979c21780627b0a00ae848af56f05f59a7b433
ajinkya-deshmukh <ajinkya.deshmukh04@gmail.com>
2022-03-20 11:37:26 +0530

    Fixed home page locators and test

--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -1,23 +1,38 @@
 package Pages;
 
+import Utils.SpinUpBrowser;
+import io.netty.util.internal.logging.Log4JLoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.impl.Log4jLogEvent;
+import org.apache.maven.plugin.logging.Log;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 public class HomePage {
+//    private static org.apache.log4j.Logger log = Logger.getLogger(LogClass.class);
+//    Logger logger = (Logger) LogManager.getLogger(HomePage.class);
+    private static Logger logger = (Logger) LogManager.getLogger(HomePage.class);
+
+//    Log4JLoggerFactory logger = new Log4JLoggerFactory();
 
     @FindBy(id = "college-finder")
     WebElement btnCollegeFinder;
 
-    @FindBy(linkText = "/feed")
+    @FindBy(css = "a[href='/feed']")
     WebElement btnHome;
 
-    @FindBy(xpath = "//h3[contains(text(),'If studying abroad is your dream, ')]")
+    @FindBy(xpath = "//h3[contains(text(),'If studying abroad is your dream')]")
     WebElement lblHomeTxt;
 
     @FindBy(id = "login-user-button")
     WebElement btnLogin;
+
+    SoftAssert softassert = new SoftAssert();
 
     WebDriver driver;
 
@@ -27,10 +42,21 @@ public class HomePage {
     }
 
     public void verifyHomePage(){
-        btnCollegeFinder.isDisplayed();
-//        btnHome.isDisplayed();
-        lblHomeTxt.isDisplayed();
-        btnLogin.isDisplayed();
+        softassert.assertTrue(btnCollegeFinder.isDisplayed());
+        softassert.assertTrue(btnHome.isDisplayed());
+//        softassert.assertTrue(lblHomeTxt.isDisplayed());
+        softassert.assertTrue(btnLogin.isDisplayed());
+        softassert.assertAll();
+        logger.info("Verified Home Page");
+    }
+
+    public void clickOnLogin(){
+        btnLogin.click();
+        logger.info("Login clicked");
+    }
+
+    public void clickOnCollegeFinder(){
+        btnCollegeFinder.click();
     }
 
 
--- src/test/java/HomePageVerification.java
+++ src/test/java/HomePageVerification.java
@@ -1,25 +1,13 @@
 import Pages.HomePage;
 import Utils.SpinUpBrowser;
-import org.openqa.selenium.WebDriver;
-import Utils.Constants;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
-public class HomePageVerification {
-
-    WebDriver driver;
-
-    public void startTest(){
-        driver = SpinUpBrowser.SpinUpBrowser("chrome", Constants.BASE_URL);
-    }
+public class HomePageVerification extends SpinUpBrowser{
 
     @Test
     public void verifyHomePage(){
-        startTest();
         HomePage homePage = new HomePage(driver);
         homePage.verifyHomePage();
     }
-
-
-
-
 }
