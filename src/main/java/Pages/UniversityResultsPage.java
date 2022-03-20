e6435b9c495341928e75f35650802e0c21114298
ajinkya-deshmukh <ajinkya.deshmukh04@gmail.com>
2022-03-20 11:39:11 +0530

    Added university results page and locators

---
+++ src/main/java/Pages/UniversityResultsPage.java
@@ -0,0 +1,39 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.asserts.SoftAssert;
+
+public class UniversityResultsPage {
+
+    @FindBy(id = "modal-headline")
+    WebElement lblJoinYocket;
+
+    @FindBy(xpath = "//a//button")
+    WebElement btnLoginDialog;
+
+
+    WebDriver driver;
+
+    private WebDriverWait wait;
+
+    SoftAssert softassert = new SoftAssert();
+
+    public UniversityResultsPage(WebDriver driver){
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 15, 50);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void verifyCollegeFinderSubmission(){
+        wait.until(ExpectedConditions.visibilityOf(lblJoinYocket));
+        softassert.assertTrue(lblJoinYocket.isDisplayed());
+        btnLoginDialog.click();
+        softassert.assertAll();
+    }
+
+}
