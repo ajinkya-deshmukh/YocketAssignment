f21c4f15f48381d0951e6ce6602b8ba9f42ed9f2
ajinkya-deshmukh <ajinkya.deshmukh04@gmail.com>
2022-03-20 15:45:16 +0530

    Added cross browser testing options

--- src/main/java/Utils/SpinUpBrowser.java
+++ src/main/java/Utils/SpinUpBrowser.java
@@ -4,19 +4,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
 
 public class SpinUpBrowser {
 
     public static WebDriver driver;
 
+    @Parameters("browser")
     @BeforeMethod
-    public WebDriver start(){
-        driver = SpinUpBrowser.SpinUpBrowser("chrome", Constants.BASE_URL);
-        return driver;
+    public WebDriver start(String browser){
+        if (browser.equalsIgnoreCase("chrome")){
+            driver = SpinUpBrowser.SpinUpBrowser("chrome", Constants.BASE_URL);
+            return driver;
+        } else if (browser.equalsIgnoreCase( "firefox")){
+            driver = SpinUpBrowser.SpinUpBrowser("firefox", Constants.BASE_URL);
+            return driver;
+        }
+        else {
+            driver = SpinUpBrowser.SpinUpBrowser("chrome", Constants.BASE_URL);
+            return driver;
+        }
     }
 
     @AfterMethod
@@ -26,19 +33,20 @@ public class SpinUpBrowser {
 
     public static WebDriver SpinUpBrowser(String browsername , String url)
     {
+        System.out.println("This is the bwoser name inside " + browsername);
         if(browsername.equalsIgnoreCase("Firefox"))
         {
-            // Set the path for geckodriver.exe
-            System.setProperty("webdriver.firefox.marionette","Drivers/geckodriver");
+            // Set the path for geckodriver
+            System.setProperty("webdriver.gecko.driver","Drivers/geckodriver");
             driver = new FirefoxDriver();
         }
         else if(browsername.equalsIgnoreCase("Chrome"))
         {
-            // Set the path for chromedriver.exe
+            // Set the path for chromedriver
             System.setProperty("webdriver.chrome.driver","Drivers/chromedriver");
             driver = new ChromeDriver();
+            driver.manage().window().maximize();
         }
-        driver.manage().window().maximize();
         driver.get(url);
         return driver;
     }
